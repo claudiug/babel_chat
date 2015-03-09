cc8858a816181da3a5ba5c541f29c91d5806c42e
claudiug <claudiu.garba@gmail.com>
2015-03-10 00:13:18 +0100

    html and form for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(params.require(:user).permit(:username))
+    if @user.valid?
+      session[:username] = @user.username
+      flash[:info] = "Welcome #{@user.username}"
+      redirect_to #TODO chat here
+    else
+      flash[:error] = "Please fix the errors"
+      render :new
+    end
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User
+  include ActiveModel::Model
+  attr_accessor :username
+  validates :username, presence: true
+  validates :username, length: {within: 3..30}
+end
